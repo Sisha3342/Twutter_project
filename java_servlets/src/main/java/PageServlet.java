8d81b9a703a0cf68ae851e8c4046dd3c3df753dc
spalanevich <alexander.palanevich@gmail.com>
2020-03-15 02:34:01 +0300

    Refactored page servlet

--- java_servlets/src/main/java/PageServlet.java
+++ java_servlets/src/main/java/PageServlet.java
@@ -1,11 +1,12 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class PageServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        response.setStatus(response.SC_MOVED_TEMPORARILY);
-        response.setHeader("Location", "page.jsp");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("page.jsp").forward(request, response);
     }
 }
