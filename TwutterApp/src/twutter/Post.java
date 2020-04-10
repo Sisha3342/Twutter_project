d27b159e3d39a735562f3d8e11d30f9e6e49eb2f
spalanevich <alexander.palanevich@gmail.com>
2020-04-10 19:03:33 +0300

    Added 'Post' and 'PostList' classes for working with posts. Added setters/getters for 'Post' and constructor for 'PostsList' which creates list with two test posts

---
+++ TwutterApp/src/twutter/Post.java
@@ -0,0 +1,79 @@
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+    private String id;
+    private String description;
+    private Date createdAt;
+    private String author;
+    private String photoLink;
+    private List<String> hashTags;
+    private List<String> likes;
+
+    public Post(String id, String description, Date createdAt, String author, String photoLink,
+                List<String> hashTags, List<String> likes) {
+        this.id = id;
+        this.description = description;
+        this.createdAt = (Date)createdAt.clone();
+        this.author = author;
+        this.photoLink = photoLink;
+        this.hashTags = List.copyOf(hashTags);
+        this.likes = List.copyOf(likes);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = (Date)createdAt.clone();
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPhotoLink(String photoLink) {
+        this.photoLink = photoLink;
+    }
+
+    public void setHashTags(List<String> hashTags) {
+        this.hashTags = List.copyOf(hashTags);
+    }
+
+    public void setLikes(List<String> likes) {
+        this.likes = List.copyOf(likes);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPhotoLink() {
+        return photoLink;
+    }
+
+    public List<String> getHashTags() {
+        return hashTags;
+    }
+
+    public List<String> getLikes() {
+        return likes;
+    }
+}
---
+++ TwutterApp/src/twutter/PostsList.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class PostsList {
+    private List<Post> posts;
+
+    public PostsList() {
+        posts = new ArrayList<>();
+
+        this.add(new Post("1", "abc", new Date(), "Sisha", "link",
+                List.of("tag1", "tag2"), List.of("Alex", "Misha")));
+        this.add(new Post("2", "abc", new Date(), "Alex", "link123",
+                List.of("tag1", "tag3"), List.of("Alex")));
+    }
+
+    public Post getPost(String id) {
+        return posts.get(Integer.parseInt(id));
+    }
+
+    public static boolean validate(Post post) {
+        return  post.getId() != null && post.getId().length() > 0 &&
+                post.getDescription() != null && post.getDescription().length() < 200 &&
+                post.getCreatedAt() != null &&
+                post.getAuthor() != null && post.getAuthor().length() > 0;
+    }
+
+    public boolean add(Post post) {
+        if (validate(post)) {
+            posts.add(post);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean remove(String id) {
+        return posts.removeIf(post -> post.getId().equals(id));
+    }
+
+    public List<Post> addAll(List<Post> newPosts) {
+        List<Post> invalidPosts = new ArrayList<>();
+
+        for (Post post: newPosts) {
+            if (validate(post)) {
+                posts.add(post);
+            }
+            else {
+                invalidPosts.add(post);
+            }
+        }
+
+        return invalidPosts;
+    }
+
+    public void clear() {
+        posts.clear();
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+}
