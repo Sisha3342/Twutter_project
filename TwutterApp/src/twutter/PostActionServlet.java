265d73d681572fc259ce573a8f0b3ef4aeea7024
spalanevich <alexander.palanevich@gmail.com>
2020-04-10 22:36:47 +0300

    Added 'Post' method for adding posts to total posts list. Suppose that post is passed as json string

--- TwutterApp/src/twutter/PostActionServlet.java
+++ TwutterApp/src/twutter/PostActionServlet.java
@@ -1,10 +1,12 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class PostActionServlet extends HttpServlet {
     private static PostsList posts = new PostsList();
@@ -25,5 +27,13 @@ public class PostActionServlet extends HttpServlet {
         String postId = req.getParameter(parameterName);
 
         resp.getWriter().print(String.valueOf(posts.remove(postId)));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String body = req.getReader().readLine();
+        Post postToAdd = (new Gson()).fromJson(body, Post.class);
+
+        resp.getWriter().write(String.valueOf(posts.add(postToAdd)));
     }
 }
