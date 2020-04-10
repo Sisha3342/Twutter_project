de6427553b96db981b5fbb56e9904f71c0aa51d4
spalanevich <alexander.palanevich@gmail.com>
2020-04-10 23:44:56 +0300

    Added servlets from previous tasks. Also replaced JSON in CheckServlet with Gson

---
+++ TwutterApp/src/twutter/CheckServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+public class CheckServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+
+        Gson gson = new Gson();
+
+        response.getOutputStream().println(gson.toJson("success"));
+    }
+}
---
+++ TwutterApp/src/twutter/GetNameServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GetNameServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("Name is " + request.getParameter("name"));
+    }
+}
