f12ebef5a1237fce75826068bfef92ff8f85dccb
spalanevich <alexander.palanevich@gmail.com>
2020-04-10 20:40:10 +0300

    Added 'PostAction' servlet for processing requests with posts. Extended method 'doGet' for gettin post from posts by id

---
+++ TwutterApp/src/twutter/PostActionServlet.java
@@ -0,0 +1,22 @@
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PostActionServlet extends HttpServlet {
+    private static PostsList posts = new PostsList();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String parameterName = "id";
+        String postId = req.getParameter(parameterName);
+
+        Post postToReturn = posts.getPost(postId);
+
+        resp.getWriter().print((new Gson()).toJson(postToReturn));
+    }
+
+}
